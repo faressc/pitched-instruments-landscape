a28199688e168849c9ba561aa9ede0579f0d8b01
Fares Schulz <schulzfares@gmail.com>
2025-04-07 21:36:07 +0200

    updated generate scatter script

--- source/generate_scatter.py
+++ source/generate_scatter.py
@@ -35,14 +35,14 @@ batch_size = 32
 
 
 
-print(f"Creating the valid dataset and dataloader with db_path: {cfg.train.db_path_valid}")
-train_dataset = MetaAudioDataset(db_path=cfg.train.db_path_train, max_num_samples=-1, has_audio=False)
+print(f"Creating the valid dataset and dataloader with db_path: {cfg.train.db_path_train}")
+train_dataset = MetaAudioDataset(db_path=cfg.train.db_path_train, max_num_samples=-1, has_audio=False, fast_forward_keygen=True)
 sampler = CustomSampler(dataset=train_dataset, pitch=cfg.train.pitch, velocity=cfg.train.velocity, max_inst_per_family=cfg.train.max_inst_per_family, shuffle=False)
 dl = DataLoader(train_dataset,
                 batch_size=batch_size,
                 sampler=sampler,
                 drop_last=False,
-                # num_workers=cfg.train.num_workers,
+                num_workers=cfg.train.num_workers,
                 )
 
 
