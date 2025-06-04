fixed logvar scaling for the transformer variance

source/train_transformer.py
@@ -264,7 +264,8 @@ def main():
             vae_output = condition_model.forward(emb)
             timbre_cond = vae_output[1].detach()
             # TODO: Applying the VAE variation to the timbre condition (is this logvar or not?)
-            # timbre_cond += vae_output[2].detach()
+            std = torch.exp(0.5 * vae_output[2].detach())
+            timbre_cond += torch.randn_like(timbre_cond) * std
             pitch_cond = vae_output[4].detach()
 
             # concatenating timbre and pitch condition for putting into encoder of transformer